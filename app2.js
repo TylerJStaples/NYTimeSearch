538714eee9bec64d8e5aba69acb1e9c70c4a7ef8
Tyler Staples <TylerJStaples@gmail.com>
2018-04-14 14:01:23 -0400

    Ajax call included

--- app2.js
+++ app2.js
@@ -1,2 +1,10 @@
-<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
-console.log("This is the first file");
+var QueryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+QueryURL += '?' + $.param({
+  'api-key': "88523d24c2e24d70a4d7adbeceffc8d9"
+});
+$.ajax({
+    url: QueryURL,
+    method: "GET",
+}).then(function(response) {
+    console.log(response);
+});
