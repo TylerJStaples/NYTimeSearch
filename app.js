1efe9ff3231f69f1b568150f1856ea00018b0332
taw1313 <taw1313@gmail.com>
2018-04-14 13:37:53 -0400

    2nd response display

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
+jQuery(document).ready(function(){
+    $.getScript('app2.js');
+});
+
 //Global variables
 var results = $("#results");
 
 function buildResponseArea() {
-    var newRow = $("<row>");
-    newRow.addClass("temp-class-response");
+    var jq_newRow = $("<row>");
+    jq_newRow.addClass("temp-class-response");
 
+    jq_newResponseNum = $("<h2>");
+    jq_newResponseNum.text(response-id);
 
-    
+    jq_newResponseP = $("<p>");
+    jq_newResponseP.text(response);
+
+    jq_newRow.append(jq_newResponseNum);
+    jq_newRow.append(jq_newResponse);
+
+    results.append(jq_newRow);
 }
+
+// #search-term;
+// #search-number;
+// #search-start-year;
+// #search-end-year;
